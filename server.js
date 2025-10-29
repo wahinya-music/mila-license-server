d781f1d58a31e0c0d998049a304986b326620fa4
Wahinya Mwirikia <wahinyamwirikia@Wahinyas-iMac.local>
2025-10-29 14:53:38 +0300

    update license server

--- server.js
+++ server.js
@@ -46,4 +46,8 @@ app.get("/", (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
